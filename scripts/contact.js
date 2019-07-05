96dd943da87deb4903f99ca5d1292204c3c7d661
Teddy <koolaidbelly@gmail.com>
2019-07-05 01:01:19 -0400

    Update contact.js
    added js code

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,4 +1,33 @@
 $(document).ready(function(){
+    // Get the form.
+    var form = $('#contact-form');
 
+    // Get the messages div.
+    var formMessages = $('#form-messages');
 
+    // Set up an event listener for the contact form.
+    $(form).submit(function(event) {
+        // Stop the browser from submitting the form.
+        event.preventDefault();
+    });
+    // Serialize the form data.
+    var formData = $(form).serialize();
+    // Submit the form using AJAX.
+    $.ajax({
+        type: 'POST',
+        url: $(form).attr('action'),
+        data: formData
+    }).done(function(response) {
+        // Make sure that the formMessages div has the 'success' class.
+        $(formMessages).removeClass('error');
+        $(formMessages).addClass('success');
+    
+        // Set the message text.
+        $(formMessages).text(response);
+    
+        // Clear the form.
+        $('#name').val('');
+        $('#email').val('');
+        $('#message').val('');
+    })
 })
